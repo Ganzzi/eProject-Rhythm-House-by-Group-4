df5d6a51c94ada041e325bc3d16eb61e86084b2f
imduyan <61102692+imduyan@users.noreply.github.com>
2022-12-08 01:20:49 +0700

    add button scroll up

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+//Button Scroll Up to top
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//Initial angular app
 let app = angular.module("myApp", ["ngRoute"]);
 
 localStorage.setItem("logInStatus", "false");
